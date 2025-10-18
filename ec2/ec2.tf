f0c8c2d73a3bf33f7b0531c91b397d646aad844a
bharathchandu302 <bharathchandu302@gmail.com>
2025-10-18 22:23:25 +0530

    creating ec2 instance by using terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "example" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow-all.id]
+
+  tags = {
+    Name = "terraform"
+    terraform = true
+  }
+}
+
+resource "aws_security_group" "allow-all" {
+  name   = "allow-all"
+
+   egress {
+    from_port        = 0 # from  port to 0
+    to_port          = 0 # to port to 0
+    protocol         = "-1" # protocall
+    cidr_blocks      = ["0.0.0.0/0"] # allow internet
+  }
+
+    ingress {
+    from_port        = 0 # from  port to 0
+    to_port          = 0 # to port to 0
+    protocol         = "-1" # protocall
+    cidr_blocks      = ["0.0.0.0/0"] # allow internet
+  }
+}
+
